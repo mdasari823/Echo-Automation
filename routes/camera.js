683fd9cd2d893842ef2cc6eb6939f79da960a744
Santiago Vargas <santiagovargas@Santiagos-MacBook-Pro.local>
2017-02-18 18:09:18 -0500

    Added status endpoints

--- routes/camera.js
+++ routes/camera.js
@@ -2,19 +2,41 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 
+router.get('/:sensor', function(req, res, next) {
+  var sensor = req.params.sensor;
+  console.log('Sensor ', sensor);
+
+  var respobj = {
+    response: 'No data for ' + sensor
+  };
+  if (global[sensor] && !_.isUndefined(global[sensor].enabled)) {
+    var enabled = global[sensor].enabled;
+    var status = (enabled) ? 'enabled' : 'disabled';
+    respobj.response = 'The ' + sensor + ' is ' + status;
+  }
+  return res.json(respobj);
+});
+
 router.post('/:sensor', function(req, res, next) {
   var sensor = req.params.sensor;
-  console.log('Sensor used', sensor);
+  console.log('Sensor ', sensor);
   var body = req.body;
 
   var respobj = {
     response: 'Bad data'
   };
   if (body && !_.isUndefined(body.enabled)) {
-    var status = (body.enabled) ? 'enabled' : 'disabled';
-    respobj.response = 'The ' + sensor + ' is ' + status;
-  } else {
-
+    var enabled = false;
+    if (body.enabled) {
+      enabled = true;
+    }
+    //Store the sensor. Globals are bad!!!
+    if (!global[sensor]) {
+      global[sensor] = {};
+    }
+    global[sensor].enabled = enabled;
+    var status = (enabled) ? 'enabled' : 'disabled';
+    respobj.response = 'The ' + sensor + ' was set to ' + status;
   }
   return res.json(respobj);
 });
