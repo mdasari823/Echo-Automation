d646e6cf606e117ad639ae6089aa3e6173f3faa0
Santiago Vargas <santiagovargas@Santiagos-MacBook-Pro.local>
2017-02-18 01:40:58 -0500

    Used tunneling

--- routes/alexa_message.js
+++ routes/alexa_message.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log('Alexa Message');
+  console.log('Received Alexa Message');
   console.log(req.body.request.intent);
+
 });
 
 module.exports = router;
