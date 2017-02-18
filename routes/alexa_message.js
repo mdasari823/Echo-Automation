a0a798ca5e0431419a31c47ab6aec1c6528a5152
Santiago Vargas <santiagovargas@Santiagos-MacBook-Pro.local>
2017-02-18 01:04:55 -0500

    Clean up of message route

--- routes/alexa_message.js
+++ routes/alexa_message.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+  console.log('Alexa Message');
+  console.log(req.body.request.intent);
 });
 
 module.exports = router;
