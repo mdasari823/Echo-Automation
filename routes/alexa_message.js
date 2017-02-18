35f10a829b76171ac2e761e1fa26b1ecb29212f7
Santiago Vargas <santiagovargas@Santiagos-MacBook-Pro.local>
2017-02-18 02:08:07 -0500

    Added automated response

--- routes/alexa_message.js
+++ routes/alexa_message.js
@@ -4,8 +4,31 @@ var router = express.Router();
 /* GET home page. */
 router.post('/', function(req, res, next) {
   console.log('Received Alexa Message');
-  console.log(req.body.request.intent);
+  console.log(req.body.request.intent.slots.NAME.value);
 
+  var resp = {
+    "version": "1.0",
+    "sessionAttributes": {},
+    "response": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "I found " + req.body.request.intent.slots.NAME.value
+      },
+      "card": {
+        "type": "Simple",
+        "title": "Friend Finder",
+        "content": "Found you!"
+      },
+      "reprompt": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": "Can I find someone else for you?"
+        }
+      },
+      "shouldEndSession": false
+    }
+  };
+  return res.send(resp);
 });
 
 module.exports = router;
