fb157328150234ebe33a64f64a64e8e42cf3b859
Santiago Vargas <santiagovargas@Santiagos-MacBook-Pro.local>
2017-02-18 20:40:35 -0500

    Added SMART home features

--- routes/alexa_message.js
+++ routes/alexa_message.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
   console.log('Received Alexa Message');
-  console.log(req.body.request.intent.slots.NAME.value);
+
+  var errorobj = {
+    status: 'error'
+  };
+  if (!req.body.request || !req.body.request.intent || !req.body.request.intent) {
+    console.log('Variable not defined', req.body.request);
+    return res.json(errorobj);
+  }
 
   var resp = {
     "version": "1.0",
@@ -12,23 +20,58 @@ router.post('/', function(req, res, next) {
     "response": {
       "outputSpeech": {
         "type": "PlainText",
-        "text": "I found " + req.body.request.intent.slots.NAME.value
+        "text": "Ask again"
       },
       "card": {
         "type": "Simple",
-        "title": "Friend Finder",
-        "content": "Found you!"
+        "title": "Cerebro Control",
+        "content": "The light status"
       },
       "reprompt": {
         "outputSpeech": {
           "type": "PlainText",
-          "text": "Can I find someone else for you?"
+          "text": "Can I do something else for you?"
         }
       },
-      "shouldEndSession": false
+      "shouldEndSession": true
     }
   };
-  return res.send(resp);
+  switch (req.body.request.intent.name) {
+    case 'LightIntent_READ':
+      console.log('LightIntent_READ');
+      console.log('slot', req.body.request.intent.slots.ENABLED);
+      resp.response.outputSpeech.text = 'I think the lights are off.';
+      if (!_.isUndefined(global.light)) {
+        console.log('Here');
+        resp.response.outputSpeech.text = (global.light.status) ? 'It\'s bright in here': 'I\m in the dark';
+      }
+      break;
+    case 'LightIntent_WRITE':
+      console.log('Write');
+      if (!global.light) {
+        global.light = {};
+      }
+      var command = req.body.request.intent.slots.ENABLED;
+      console.log('Command', command);
+      global.light.command = command;
+      resp.response.outputSpeech.text = 'I turned ' + command.value + ' the light.';
+      break;
+    case 'MotionIntent': //Could only read
+      console.log('Motion');
+      resp.response.outputSpeech.text = 'I don\'t know if there\'s anyone at home.';
+      if (!_.isUndefined(global.motion)) {
+        console.log('Here');
+        resp.response.outputSpeech.text = (global.motion.status) ? 'There\'s a monster in the closet': 'You\'re in a safe zone';
+      }
+      break;
+    default:
+      console.log('Default');
+      break;
+  }
+  console.log(resp.response.outputSpeech.text);
+
+  console.log(resp);
+  return res.json(resp);
 });
 
 module.exports = router;
--- routes/camera.js
+++ routes/camera.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 
-router.get('/:sensor', function(req, res, next) {
-  var sensor = req.params.sensor;
-  console.log('Sensor ', sensor);
+router.get('/light/command', function(req, res, next) {
+  console.log('Light Command ');
 
   var respobj = {
-    response: 'No data for ' + sensor
+    command: 'nothing'
   };
-  if (global[sensor] && !_.isUndefined(global[sensor].enabled)) {
-    var enabled = global[sensor].enabled;
-    var status = (enabled) ? 'enabled' : 'disabled';
-    respobj.response = 'The ' + sensor + ' is ' + status;
+  if (global.light && !_.isUndefined(global.light.command) && global.light.command != 'nothing') {
+    console.log(global.light.command != 'nothing');
+    var command = global.light.command;
+    var status = (command) ? 'on' : 'off';
+    respobj.command = status;
   }
+  console.log('Light command', respobj.command);
   return res.json(respobj);
 });
 
@@ -21,22 +22,26 @@ router.post('/:sensor', function(req, res, next) {
   var sensor = req.params.sensor;
   console.log('Sensor ', sensor);
   var body = req.body;
+  console.log('Body', body);
 
   var respobj = {
     response: 'Bad data'
   };
   if (body && !_.isUndefined(body.enabled)) {
     var enabled = false;
-    if (body.enabled) {
+    if (body.enabled == 'true') {
+      console.log('here', body.enabled);
       enabled = true;
     }
     //Store the sensor. Globals are bad!!!
     if (!global[sensor]) {
       global[sensor] = {};
     }
-    global[sensor].enabled = enabled;
+    global[sensor].status = enabled;
+    console.log('The ' + sensor + ' was ' + global[sensor].status);
     var status = (enabled) ? 'enabled' : 'disabled';
-    respobj.response = 'The ' + sensor + ' was set to ' + status;
+    respobj.response = 'The ' + sensor + ' is ' + status;
+    console.log('The ' + sensor + ' is ' + status);
   }
   return res.json(respobj);
 });
