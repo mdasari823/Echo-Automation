b4cd5e3b3f289395ede643bae7ba2e0051bfc09d
Santiago Vargas <santiagovargas@Santiagos-MacBook-Pro.local>
2017-02-18 22:16:08 -0500

    Modified responses

--- routes/alexa_message.js
+++ routes/alexa_message.js
@@ -61,7 +61,7 @@ router.post('/', function(req, res, next) {
       resp.response.outputSpeech.text = 'I don\'t know if there\'s anyone at home.';
       if (!_.isUndefined(global.motion)) {
         console.log('Here');
-        resp.response.outputSpeech.text = (global.motion.status) ? 'There\'s a monster in the closet': 'You\'re in a safe zone';
+        resp.response.outputSpeech.text = (global.motion.status) ? 'There\'s a monster in the closet': 'I am all by myself';
       }
       break;
     default:
